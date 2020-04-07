e776801b8aa0fe67efa8eeac6345b367cf37ffba
Holly <milstowers@gmail.com>
2020-04-07 19:57:13 +0100

    this was difficult, and i still should add more, but later

--- app.js
+++ app.js
@@ -1,105 +1,181 @@
 const API = require("./lib/API");
 const readlineSync = require("readline-sync");
+const films = API.read("films");
 
-function calculateAverageRating(book) {
-  let total = 0;
-  for (const review of book.reviews) {
-    total += parseInt(review.rating);
-  }
-  return total / book.reviews.length;
+let selections = {
+  id: "",
+  film: "",
+  time: "",
+  tickets: ""
 }
 
-function displayBooksSummary(books) {
-  for (const book of books) {
-    // if ths book has some reviews
-    if (book.reviews.length > 0) {
-      console.log(
-        `--- ${book.id}: ${book.title}, rating: ${calculateAverageRating(book)}`
-      );
+function filmCatalog(films) {
+
+  for (const film of films) {
+    console.log(`  --${film.id}: ${film.title}`);
+  }
+
+  console.log("                               ");
+  let choice = readlineSync.questionInt("Pick a film from the list:   ");
+  console.log("                               ");
+
+  if (choice <= 0 || choice >= 6) {
+    console.log("Sorry we didn't recognise that choice! Please choose again");
+    console.log("                               ");
+    filmCatalog(films);
+  } else {
+    selections.id = `${films[(choice - 1)].id}`;
+    selections.film = `${films[(choice - 1)].title}`;
+
+    console.log(`  --${films[(choice - 1)].title}`);
+    console.log(`  --Length: ${films[(choice - 1)].duration} mins`)
+    console.log(`  --Rating: ${films[(choice - 1)].ageRating}`);
+    console.log("                               ");
+
+    yesNo()
+  }
+}
+
+function yesNo() {
+  let quest = readlineSync.keyInYN(`Would you like to book ${selections.film}?`);
+  if (quest === false) {
+    console.log("                               ");
+    filmCatalog(films);
+    console.log("                               ");
+  } else if (quest === true) {
+    console.log("                               ");
+    viewShowingTimes(films, selections);
+    console.log("                               ");
+  } else {
+    yesNo();
+  }
+}
+
+function viewShowingTimes(films, selections) {
+  let num = 1;
+  for (const time of films[selections.id - 1].times) {
+    console.log(`  --${num}: ${time}`);
+    num++
+  }
+
+  console.log("                               ");
+  let choice = readlineSync.questionInt("Pick a time from the list:   ");
+  console.log("                               ");
+
+  if (choice <= 0 || choice >= num) {
+    console.log("Sorry we didn't recognise that choice! Please choose again");
+    console.log("                               ");
+    viewShowingTimes(films, selections);
+  } else {
+    selections.time = `${films[selections.id - 1].times[(choice - 1)]}`;
+    console.log("**     Your selection:     **");
+    console.log(`Film:  ${selections.film}`);
+    console.log(`Time:  ${films[selections.id - 1].times[(choice - 1)]}`);
+    console.log("                               ");
+
+    yesNo2()
+  }
+}
+
+function yesNo2() {
+  let quest = readlineSync.keyInYN(`Is this right?`);
+  if (quest === false) {
+    console.log("                               ");
+    mainMenu();
+    console.log("                               ");
+  } else if (quest === true) {
+    console.log("                               ");
+    sellTicket(films, selections);
+  } else {
+    yesNo2();
+  }
+}
+
+function sellTicket(films, selections) {
+  let choice = readlineSync.questionInt("How many tickets would you like (max 10):  ");
+  console.log("                               ");
+  /*
+  film.seating.rows
+  
+  let sold = films[selections.id-1].seating.rows;
+  let totseats = rows * rowSeats;
+  
+  let rows = films[selections.id-1].seating.rows;
+  let rowSeats = films[selections.id-1].seating.seats;
+  
+  console.log(sold);
+  console.log(remainingSeats);
+  */
+
+  if (choice <= 0 || choice > 10) {
+    console.log("Sorry selection too high! Please try again");
+    console.log("                               ");
+    sellTicket(films, selections);
+  } /*else if (choice >= (remainingSeats - sold)){
+    console.log("Sorry these seats are sold! Please try again");
+    console.log("                               ");
+    mainMenu();
+    */
+  else {
+    selections.tickets = choice;
+
+    console.log("**     Your selection:     **");
+    console.log(`Film:       ${selections.film}`);
+    console.log(`Time:       ${selections.time}`);
+    console.log(`ticket no:  ${selections.tickets}`);
+    console.log("                               ");
+
+    yesNo3()
+  }
+
+  function yesNo3() {
+    let quest = readlineSync.keyInYN(`Is this right?`);
+    if (quest === false) {
+      console.log("                               ");
+      mainMenu();
+      console.log("                               ");
+    } else if (quest === true) {
+      finished();
     } else {
-      console.log(`--- ${book.id}: ${book.title}, no reviews yet!`);
+      yesNo3();
     }
   }
+  //let num = 1;
+  //for (const avail of films[selections.id-1].ticketsSold.seatSold){
+  //  console.log(`  --${num}: Seats available `+ (availability.rows * availability.seats))
+  //  num++;
+  //}
 }
 
-function displayBookDetails(book) {
-  console.log(`-- ${book.title} --`);
-  for (const review of book.reviews) {
-    console.log(`${review.content} - Rating: ${review.rating}`);
-  }
-}
-
-function chooseABook(books) {
-  // display each ID and title
-  for (const book of books) {
-    console.log(`--- ${book.id}: ${book.title}`);
-  }
-
-  // user inputs an ID number
-  const bookChoice = readlineSync.question(
-    "Which number book would you like to review? "
-  );
-  const book = API.read("books", bookChoice);
-
-  // if the API can't find that book
-  // run chooseABook again
-  if (book !== undefined) {
-    return book;
-  } else {
-    console.log("Ooops we can't find that book!");
-    return chooseABook(books);
-  }
+function finished() {
+  console.log("                                  ");
+  console.log("----------------------------------");
+  console.log("**  Your booking is confirmed!  **");
+  console.log("----------------------------------");
+  console.log("**   Thanks for booking with    **");
+  console.log("**      Mirrormax Cinema        **");
+  console.log("----------------------------------");
+  console.log("   We hope you enjoy your film! **");
+  console.log("----------------------------------");
 }
 
 function mainMenu() {
-  console.log("----------------");
-  console.log("---- AMAZON ----");
-  console.log("----------------");
-  console.log("1. View our books");
-  console.log("2. Leave a review");
-  console.log("----------------");
+  console.log("-------------------------------");
+  console.log("**     Mirrormax Cinema      **");
+  console.log("-------------------------------");
+  console.log("**    View film selection    **");
+  console.log("-------------------------------");
 
-  const choice = readlineSync.question("Please choose an option ");
+  const choice = readlineSync.questionInt("Please press 1 to see films: ");
 
-  if (choice === "1") {
-    console.log("-----------------");
-    console.log("- ALL OUR BOOKS -");
-    console.log("-----------------");
+  if (choice === 1) {
+    console.log("                               ");
+    console.log("-------------------------------");
+    console.log("**     Our latest films:     **");
+    console.log("-------------------------------");
+    console.log("                               ");
 
-    // get all books
-    const books = API.read("books");
-    displayBooksSummary(books);
-
-    // return to main menu
-    mainMenu();
-  } else if (choice === "2") {
-    console.log("-----------------");
-    console.log("- CHOOSE A BOOK -");
-    console.log("-----------------");
-
-    const books = API.read("books");
-    const book = chooseABook(books);
-    displayBookDetails(book);
-
-    // Input review details
-    const rating = readlineSync.question("What is your rating? ");
-    const content = readlineSync.question("Please write your review ");
-
-    // add the new review to the book reviews
-    book.reviews.push({
-      rating: rating,
-      content: content
-    });
-
-    // update the book in the API
-    API.update("books", book);
-
-    console.log("----------------------------");
-    console.log("Thanks for leaving a review!");
-    console.log("----------------------------");
-
-    // return to main manu
-    mainMenu();
+    filmCatalog(films);
   } else {
     console.log("Sorry we didn't recognise that choice!");
     mainMenu();
@@ -107,3 +183,6 @@ function mainMenu() {
 }
 
 mainMenu();
+
+
+
